a89babed70ec70caaf824473176efe2a85bfb112
Awais-Dev99 <pakstd449@gmail.com>
2026-04-25 14:52:47 +0500

    comment test

--- Task Management  System/src/app/api/test/route.ts
+++ Task Management  System/src/app/api/test/route.ts
@@ -1,17 +1,17 @@
-import { prisma } from "@/lib/db";
+// import { prisma } from "@/lib/db";
 
-export async function GET() {
-  try {
-    const users = await prisma.user.findMany();
+// export async function GET() {
+//   try {
+//     const users = await prisma.user.findMany();
 
-    return Response.json({
-      success: true,
-      data: users,
-    });
-  } catch (error) {
-    return Response.json({
-      success: false,
-      error: "Database connection failed",
-    });
-  }
-}
+//     return Response.json({
+//       success: true,
+//       data: users,
+//     });
+//   } catch (error) {
+//     return Response.json({
+//       success: false,
+//       error: "Database connection failed",
+//     });
+//   }
+// }
